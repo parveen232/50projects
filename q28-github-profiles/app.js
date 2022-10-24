566099ece1d6306e599717127366e26588658444
parveen232 <parveen297m210@gmail.com>
2022-10-24 15:06:19 +0530

    adding repos to card & null display bug removed

--- q28-github-profiles/app.js
+++ q28-github-profiles/app.js
@@ -9,6 +9,7 @@ async function getUser(username) {
         const { data } = await axios(APIURL + username);
 
         createUserCard(data);
+        getRepos(username);
     } catch (err) {
         if (err.response.status == 404) {
             createErrorCard('No profile with this username');
@@ -16,15 +17,27 @@ async function getUser(username) {
     }
 }
 
+async function getRepos(username) {
+    try {
+        const { data } = await axios(APIURL + username + '/repos?sort=created');
+
+        addReposToCard(data);
+    } catch (err) {
+        createErrorCard('Problem fetching repos');
+    }
+}
+
 function createUserCard(user) {
+    const userID = user.name || user.login
+    const userBio = user.bio ? `<p>${user.bio}</p>` : ''
     const cardHTML = `
     <div class="card">
             <div>
                 <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
             </div>
             <div class="user-info">
-                <h2>${user.name}</h2>
-                <p>${user.bio}</p>
+                <h2>${userID}</h2>
+                ${userBio}
 
                 <ul>
                     <li>${user.followers} <strong>Followers</strong></li>
@@ -38,6 +51,22 @@ function createUserCard(user) {
     `;
 
     main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos');
+
+    repos
+        .slice(0, 5)
+        .forEach(repo => {
+            const repoEl = document.createElement('a');
+            repoEl.classList.add('repo');
+            repoEl.href = repo.html_url;
+            repoEl.target = '_blank';
+            repoEl.innerText = repo.name;
+
+            reposEl.appendChild(repoEl);
+        })
 }
 
 function createErrorCard(msg) {
