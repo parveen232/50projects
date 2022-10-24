718b5a959c2795f70531353cf12d8c95455ef743
parveen232 <parveen297m210@gmail.com>
2022-10-24 12:01:20 +0530

    fetching profile data with axios

---
+++ q28-github-profiles/app.js
@@ -0,0 +1,32 @@
+const APIURL = 'https://api.github.com/users/';
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+// function getUser(username) {
+//     axios(APIURL + username)
+//         .then(res => console.log(res.data))
+//         .catch(err => console.log(err))
+// }
+
+async function getUser(username) {
+    try {
+        const { data } = await axios(APIURL + username);
+
+        console.log(data);
+    } catch (err) {
+        console.log('ERROR!', err);
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user) {
+        getUser(user);
+
+        search.value = '';
+    }
+})
